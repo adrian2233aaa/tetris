28e848b3209a919e3b2ccd06c68666b3bf19c1e3
adrian2233aaa <160290552+adrian2233aaa@users.noreply.github.com>
2024-05-10 05:02:32 +0000

    Version 0

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # tetris
+# Mi Proyecto
+
+Este es un proyecto basado en el juego arcade tetris.
+
+## Descripción
+
+El propósito de este proyecto es demostrar el juego llamado tetris
+
+## Instalación
+
+Para instalar este proyecto, simplemente clona el repositorio
+
+## Uso
+
+Para utilizar este proyecto, simplemente usa el coamdo make runPantalla o make run para que corra.
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <thread>
+#include <fstream>
+#include <string>
+#include <Dibujo.hpp>
+
+using namespace std;
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+    Dibujo moto("assets/images/moto.txt");
+    Dibujo bloque("assets/images/bloque.txt");
+
+    int fotograma = 0;
+    while (true)
+    {
+        this_thread::sleep_for(0.1s);
+        fotograma++;
+        
+        Element dibujo = hbox( moto.GetElement(),bloque.GetElement());
+        Screen pantalla = Screen::Create(Dimension::Full());
+        Render(pantalla, dibujo);
+        pantalla.Print();
+        cout << pantalla.ResetPosition();
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
